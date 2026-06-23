ca6e50811bd01f2704f54960f15b4433cda6af25
Felipe Silva <97602680+felipesmatos@users.noreply.github.com>
2026-06-23 17:30:34 -0300

    Update pet_feeder.c
    modificando a lógica de envio

--- pet_feeder.c
+++ pet_feeder.c
@@ -14,6 +14,9 @@ const char* password = "";
 // =====================
 const char* TOKEN = "";
 
+unsigned long ultimoEnvio = 0;
+const unsigned long intervaloEnvio = 1000;
+
 // =====================
 // HARDWARE
 // =====================
@@ -36,13 +39,19 @@ const float FATOR = 42.0;
 
 int leitura;
 
-int racao;
-int agua;
+float racao;
+float agua;
 
-int racao_liberada;
-float doseRacao = 40.0;
+float agua_liberada;
+float racao_liberada;
+
+float racao_consumida;
+float agua_consumida;
+
+float doseRacao = 40.0; // para testes
 
 bool alimentando = true;
+bool hidratando = true;
 
 Servo servoRacao;
 
@@ -50,64 +59,131 @@ Servo servoRacao;
 // CONTROLE
 // =====================
 
-/*float lerPesoRacao() {
+void conectarWiFi() {
 
-  long leitura = balancaRacao.read();
+  Serial.print("Conectando ao WiFi");
 
-  if (leitura < 0)
-    leitura = 0;
+  WiFi.begin(ssid, password);
 
-  return (leitura / FATOR) / 10;
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println();
+  Serial.println("WiFi conectado!");
+  Serial.print("IP: ");
+  Serial.println(WiFi.localIP());
 }
 
-float lerPesoAgua() {
+void enviarTelemetriaRacao() {
 
-  long leitura = balancaAgua.read();
+  if (WiFi.status() != WL_CONNECTED)
+    return;
 
-  if (leitura < 0)
-    leitura = 0;
+  racao_consumida = racao_liberada - racao;
 
-  return (leitura / FATOR) / 10;
-}*/
+  if(racao_consumida < 0)
+    racao_consumida = 0;
 
-int lerPesoRacao() {
+  HTTPClient http;
+
+  http.begin("https://api.tago.io/data");
+
+  http.addHeader("Content-Type", "application/json");
+  http.addHeader("Device-Token", TOKEN);
+
+  String payload = "[";
+
+  payload += "{\"variable\":\"racao_liberada\",\"value\":";
+  payload += String(racao_liberada, 2);
+  payload += "},";
+
+  payload += "{\"variable\":\"racao_consumida\",\"value\":";
+  payload += String(racao_consumida, 2);
+  payload += "}";
+
+  payload += "]";
+
+  http.POST(payload);
+
+  http.end();
+}
+
+void enviarTelemetriaAgua() {
+
+  if (WiFi.status() != WL_CONNECTED)
+    return;
+
+  agua_consumida = agua_liberada - agua;
+
+  if(agua_consumida < 0)
+    agua_consumida = 0;
+
+  HTTPClient http;
+
+  http.begin("https://api.tago.io/data");
+
+  http.addHeader("Content-Type", "application/json");
+  http.addHeader("Device-Token", TOKEN);
+
+  String payload = "[";
+
+  payload += "{\"variable\":\"agua_liberada\",\"value\":";
+  payload += String(agua_liberada, 2);
+  payload += "},";
+
+  payload += "{\"variable\":\"agua_consumida\",\"value\":";
+  payload += String(agua_consumida, 2);
+  payload += "}";
+
+  payload += "]";
+
+  http.POST(payload);
+
+  http.end();
+}
+
+float lerPesoRacao() {
 
   long leitura = balancaRacao.read();
-
   return (leitura / FATOR) / 10;
 }
 
 int lerPesoAgua() {
 
   long leitura = balancaAgua.read();
-
   return (leitura / FATOR) / 10;
 }
 
 void alimentar(int peso){
-  servoRacao.write(60);
-  if(peso >= doseRacao){
+  if(peso < doseRacao){
+    servoRacao.write(70);
+    delay(1500);
+  }
+  else if((peso >= (doseRacao * 0.8)) && (peso < doseRacao)){
+
+    servoRacao.write(45);
+    delay(1000);
+    //racao_liberada = peso;
+
+  }else{
     servoRacao.write(0);
     racao_liberada = peso;
     alimentando = false;
   }
-  else if((peso >= (doseRacao * 0.8)) && (peso < doseRacao)){
-    servoRacao.write(45);
-    racao_liberada = peso;
-  }
+
 }
 
-void Bomba(int op){
-  if(op == 1){
-
+void Bomba(float litros){
+  if(litros < 25){
     digitalWrite(RELE_BOMBA, HIGH);
     delay(2000);
-
-  }else{
-
     digitalWrite(RELE_BOMBA, LOW);
+  }else{
+    agua_liberada = litros;
+    hidratando = false;
     Serial.println("refil de água");
-
   }
 }
 
@@ -115,7 +191,7 @@ void setup() {
 
   Serial.begin(115200);
 
-  //conectarWiFi();
+  conectarWiFi();
   pinMode(RELE_BOMBA, OUTPUT);
 
   digitalWrite(RELE_BOMBA, LOW);
@@ -133,36 +209,36 @@ void setup() {
 void loop() {
 
   racao = lerPesoRacao();
-  Serial.println("racao");
-  Serial.println(racao);
+  Serial.print("racao: ");
+  Serial.print(racao);
   Serial.println("kg");
 
   agua = lerPesoAgua();
-  Serial.println("agua");
-  Serial.println(agua);
+  Serial.print("agua:");
+  Serial.print(agua);
   Serial.println("kg");
 
+
   if(alimentando){
-
     alimentar(racao);
-    Serial.println("racao liberada");
-    Serial.println(racao_liberada);
-
-  } else {
-
-    Serial.println("enviar os dados");
-    delay(5000);
-  }
-
-  if(agua < 25){
-    Bomba(1);
-    //servoRacao.write(45);
-  }
-  else{
-    Bomba(0);
-    //servoRacao.write(0);
   }
 
 
+  if(hidratando){
+    Bomba(agua);
+  }
+  
+
+  if (millis() - ultimoEnvio >= intervaloEnvio) {
+    ultimoEnvio = millis();
+
+    if(!alimentando){
+      enviarTelemetriaRacao();
+    }
+
+    if(!hidratando){
+      enviarTelemetriaAgua();
+    }
+  }
   delay(500);
 }
